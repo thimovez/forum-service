b18567b66c009decc0949b48f839a59baeafc3da
Vasyl Kovalenko <thimovez@gmail.com>
2023-07-01 23:08:58 +0300

    setup database

---
+++ src/core/database/database.config.ts
@@ -0,0 +1,27 @@
+import { IDatabaseConfig } from './interfaces/dbConfig.interface';
+
+export const databaseConfig: IDatabaseConfig = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_DEVELOPMENT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_PRODUCTION,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+};
